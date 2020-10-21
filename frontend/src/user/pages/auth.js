6ebefab32d03e04a44836711c58a6ee72a4bde81
Geremsa <geremsa111@gmail.com>
2020-10-21 21:55:47 +0530

    switch between login and signup

--- frontend/src/user/pages/auth.js
+++ frontend/src/user/pages/auth.js
@@ -1,34 +1,98 @@
-import React from 'react'
-import Button from '../../shared/components/formelements/Button'
-import Input from '../../shared/components/formelements/input'
-import { useForm } from '../../shared/hooks/form-hooks'
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/Util/validators'
+import React, { useState } from "react";
+import Button from "../../shared/components/formelements/Button";
+import Input from "../../shared/components/formelements/input";
+import Card from "../../shared/components/UIElements/Card";
+import { useForm } from "../../shared/hooks/form-hooks";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/Util/validators";
 
 function Auth() {
-    const [formState,inputHandler] = useForm({
-        email:{
-            value:'',
-            isValid:false
+  const [isLoggedInMode, setisLoggedInMode] = useState(true);
+
+  const [formState, inputHandler, setInputData] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
+  const switchMode = () => {
+    if (isLoggedInMode) {
+      setInputData(
+        {
+         ...formState.inputs,
+          name: { value: "", isValid: false },
         },
-        password:{
-            value:'',
-            isValid:false
-        }
-    },false)
-
-    const onSubmitHandler= event=>{
-        event.preventDefault();
-        console.log(formState)
-
+        false
+      );
+    } else  {
+      setInputData(
+        {...formState.inputs,
+          name: undefined,
+        },
+        formState.inputs.email.isValid &&  formState.inputs.password.isValid
+      );
     }
-    return (
-      <form onSubmit={onSubmitHandler} className="place-form">
-          <Input id="email" element="input" type="email" label="Email" validators={[VALIDATOR_REQUIRE,VALIDATOR_EMAIL]} errorText="Enter a valid email" onInput={inputHandler}/>
-   
-          <Input id="password" element="input" type="password" label="Password" validators={[VALIDATOR_MINLENGTH(6)]} errorText="Enter a valid email" onInput={inputHandler}/>
-          <Button type="submit" disabled={!formState.isValid}>Create User</Button>
+    setisLoggedInMode((p) => !p);
+  };
+  return (
+    <Card className="place-form">
+      <h2>{isLoggedInMode ? "LOGIN" : "SIGNUP"}</h2>
+      <form onSubmit={onSubmitHandler}>
+        {!isLoggedInMode && (
+          <Input
+            id="name"
+            element="input"
+            type="text"
+            label="Name"
+            validators={[VALIDATOR_REQUIRE]}
+            errorText="Please enter a name."
+            onInput={inputHandler}
+          />
+        )}
+
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="Email"
+          validators={[VALIDATOR_REQUIRE, VALIDATOR_EMAIL]}
+          errorText="Enter a valid email"
+          onInput={inputHandler}
+        />
+
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(6)]}
+          errorText="Enter a valid email"
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          {isLoggedInMode ? "LOGIN" : "SIGNUP"}
+        </Button>
+        <Button inverse onClick={switchMode}>
+          Go to {isLoggedInMode ? "SIGNUP" : "LOGIN"}
+        </Button>
       </form>
-    )
+    </Card>
+  );
 }
 
-export default Auth
+export default Auth;
