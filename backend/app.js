c70dfc1f5094572283eadfb4c7508ebb885298cd
Geremsa <geremsa111@gmail.com>
2020-10-24 10:58:46 +0530

    error handling

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,13 @@ const app = express()
 
 //routes use
 app.use('/api/places',placesRoutes)
-
+app.use((error,req,res,next)=>{
+    if(res.headerSent) {
+        return next(error)
+    }
+    res.status(error.code || 500)
+    res.json(error.message || {message:"An unknown error occurred"})
+})
 
 
 //listen
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -21,6 +21,12 @@ router.get('/:pid',(req,res,next)=>{
     console.log("GET request in console")
     const placeId = req.params.pid //{pid:'p1'}
     const place = DUMMY_PLACES.find(p=>p.id===placeId)
+
+    if(!place){
+        const error = new Error("Couldn't find place for the provided id")
+        error.code = 404
+        return next(error)
+    }
     res.json({place})
 
 })
@@ -28,6 +34,12 @@ router.get('/users/:uid',(req,res,next)=>{
     console.log("GET request in Users")
     const userId = req.params.uid
     const user = DUMMY_PLACES.find(p=>p.creator===userId)
+
+    if(!user){
+       const error = new Error("Couldn't find place for the provided user id")
+       error.code = 404
+       return next(error)
+    }
     res.json({user})
 
 })
