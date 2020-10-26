d2fa1dd733bb7eaa208ee63276662cf7d9d9afa1
Geremsa <geremsa111@gmail.com>
2020-10-26 07:00:13 +0530

    mongoose setup for our backend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyPaser = require("body-parser");
 const httpError = require("./models/errors");
 //routes import
@@ -8,26 +9,37 @@ const userRoutes = require("./routes/userroutes");
 const app = express();
 
 //bodyPaser
-app.use(bodyPaser.json())
+app.use(bodyPaser.json());
 
 //routes use
 app.use("/api/places", placesRoutes);
 app.use("/api/users", userRoutes);
 
-
-
 //error
-app.use((req,res,next)=>{
-    const error = new httpError("Couldn't find the route", 404) //overall error
-    throw error //when synchronous / next() for asynchronous
-})
-app.use((error, req, res, next) => { //for a particular param
+app.use((req, res, next) => {
+  const error = new httpError("Couldn't find the route", 404); //overall error
+  throw error; //when synchronous / next() for asynchronous
+});
+app.use((error, req, res, next) => {
+  //for a particular param
   if (res.headerSent) {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({message:error.message} || { message: "An unknown error occurred" });
+  res.json(
+    { message: error.message } || { message: "An unknown error occurred" }
+  );
 });
 
 //listen
-app.listen(5000);
+
+const url =
+  "mongodb+srv://gdai:RGt4GDz2mcR4xs9F@cluster0.dxgwg.mongodb.net/node-database?retryWrites=true&w=majority";
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to mongoDb");
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
