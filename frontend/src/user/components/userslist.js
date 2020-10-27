54afd4eb6c80ca38bf4ff39a991e8d018396d19c
Geremsa <geremsa111@gmail.com>
2020-10-27 10:19:33 +0530

    fetching users to the ui

--- frontend/src/user/components/userslist.js
+++ frontend/src/user/components/userslist.js
@@ -22,7 +22,7 @@ function Userslist(props) {
           id={user.id}
           image={user.image}
           name={user.name}
-          placeCount={user.places}
+          placeCount={user.places.length}
         />
       ))}
     </ul>
--- frontend/src/user/pages/users.js
+++ frontend/src/user/pages/users.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import Userslist from "../components/userslist";
 
 function Users() {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Echo",
-      image:
-        "https://i.pinimg.com/736x/96/e4/af/96e4af33526e00e4f5e581bbb87e6741.jpg",
-      places: 4,
-    },
-  ];
-  return <Userslist items={USERS} />;
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
+  const [loadedUsers, setloadedUsers] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setisLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Sorry, something went wrong !!");
+        }
+        setloadedUsers(responseData.users);
+      } catch (err) {
+        console.log(err);
+        setisError(err.message);
+      }
+      setisLoading(false);
+    };
+    fetchData();
+  },[]);
+  return (
+    <>
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner asOverlay />
+        </div>
+      )}
+      {isError && (
+        <ErrorModal
+          error={isError}
+          header="An Error Occurred"
+          onClear={() => setisError(null)}
+        />
+      )}
+      {
+        loadedUsers &&
+      <Userslist items={loadedUsers} />
+      }
+    </>
+  );
 }
 
 export default Users;
