9d10df36fd4b5ae52735aba1f7c760e20e316b37
Geremsa <geremsa111@gmail.com>
2020-10-27 19:08:08 +0530

    adding place to the db

--- frontend/src/places/pages/newplace.js
+++ frontend/src/places/pages/newplace.js
@@ -8,66 +8,102 @@ import {
 } from "./../../shared/Util/validators";
 import "./placeform.css";
 import { useForm } from "../../shared/hooks/form-hooks";
+import { useHttp } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
-   
 const NewPlace = () => {
- const [formState, inputHandler]= useForm( {
-      title:{
-        value:"",
-        isValid:false
-      },
-      description:{
-        value:"",
-        isValid:false
-      },
-      address:{
-        value:"",
-        isValid:false
-      }
-    }
-    ,
-     false,
-  
-  )
+  const {
+    isLoading,
+    isError,
+    resetError,
+    isSuccess,
+    resetSuccess,
+    sendRequest,
+  } = useHttp();
 
-
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
+     sendRequest(
+      "http://localhost:5000/api/places",
+      "POST",
+      {
+        "Content-Type": "application/json",
+      },
+      JSON.stringify({
+        title: formState.inputs.title.value,
+        description: formState.inputs.description.value,
+        address: formState.inputs.address.value,
+        creator: "5f96e0f6a80fa8b1ecde4143",
+      })
+    );
     console.log(formState);
   };
 
   return (
-    <form onSubmit={onSubmitHandler} className="place-form">
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
-        onInput={inputHandler}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)."
-        onInput={inputHandler}
-      />
-      <Input
-        id="address"
-        element="input"
-        label="Address"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid address."
-        onInput={inputHandler}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        ADD PLACE
-      </Button>
-    </form>
+    <>
+      {isError && (
+        <div className="center">
+          <ErrorModal header="Error !!" error={isError} onClear={resetError} />
+        </div>
+      )}
+      {isSuccess && (
+        <ErrorModal
+          error="Place succesfully shared"
+          header="Congrtulations !!"
+          onClear={resetSuccess}
+        />
+      )}
+      <form onSubmit={onSubmitHandler} className="place-form">
+      {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title."
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid description (at least 5 characters)."
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          element="input"
+          label="Address"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid address."
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
+      </form>
+    </>
   );
 };
 
