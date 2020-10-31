5382d161792709a24b3f1d3b657b9de2ee66e9f5
Geremsa <geremsa111@gmail.com>
2020-10-31 09:28:08 +0530

    landing page setup

---
+++ frontend/src/landing/landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './landing.css';
+function LandingPage() {
+    return (
+        <>
+        <h1 className="landing-title">
+
+        </h1>
+       <div className="main-landing">
+           <div className="first-landing">
+
+           </div>
+           <div className="second-landing">
+
+           </div>
+       </div>
+       </>
+    )
+}
+
+export default LandingPage
